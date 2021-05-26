6033132966700a89984f609b182b17ecc74c6d97
sdcaglar <seedacaglar@gmail.com>
2021-05-26 09:53:37 +0300

    edited login method and added logout method

--- applications/app/static/js/gui.js
+++ applications/app/static/js/gui.js
@@ -1,10 +1,18 @@
 var app = new Vue({
-  el: '#app',
-  data: {    
-  	menuList: [
-      { 'name': 'Conference', 'iconClass': 'people_alt' },
-      { 'name': 'Network', 'iconClass': 'signal_cellular_alt'}
+  'el': '#app',
+  'data': {
+    'menuList': [
+      { 'name': 'Konferans', 'iconClass': 'people_alt' },
+      { 'name': 'Network', 'iconClass': 'signal_cellular_alt' },
     ],
-    selectedMenu: 'Conference'
-  }
+    'selectedMenu': 'Konferans',
+  },
+  'methods': {
+    logoutOnClick: function () {
+      request('delete', '/api/auth', null).then(data => {
+        data.status == 'OK' ? window.location = "/login" :
+                              this.message = "Oturum sonlandırılamadı"
+      })
+    },
+  },
 });
--- applications/app/static/js/login.js
+++ applications/app/static/js/login.js
@@ -1,45 +1,26 @@
-async function getDataAsync(url)
-{
-  let response = await fetch(url);
-  let data = await response.json()
-  return data;
-}
-
-
 var app = new Vue({
   'el': '#login-app',
   'data': {
     'username': '',
     'password': '',
     'passwordType': 'password',
-    'message': ''
+    'message': '',
   },
   'methods': {
-    'loginOnClick': function () {
-      this.message = '';
-      // data = JSON.stringify({'username': this.username, 'password': this.password});
-      // resp = request('POST', '/api/auth', [['Content-Type', 'application/json']], data);
-      fetch('/api/auth', {
-        method: 'post',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({'username': this.username, 'password': this.password})
-      }).then(r => r.json())
-        .then(data => {
-          if (data.status == 'OK') {
-            console.log('XXXX', data)
-            window.location = '/';
-          } else {
-            console.log('ERROR', data)
-            this.message = 'The username or password is incorrect';
-          }
-      });
+    loginOnClick: function () {
+      this.message = "";
+      data = {'username': this.username, 'password': this.password}
+      request('post', '/api/auth', data).then(data => {
+        data.status == 'OK' ? window.location = "/" :
+                              this.message = "Kullanıcı adı ya da parola hatalı"
+      })
     },
-  	'showPassword': function () {
-      if (this.passwordType == 'password') {
-        this.passwordType = 'text';
+    showPassword: function () {
+      if (this.passwordType == "password") {
+        this.passwordType = "text";
       } else {
-        this.passwordType = 'password';
+        this.passwordType = "password";
       }
-    }
-  }
+    },
+  },
 });
