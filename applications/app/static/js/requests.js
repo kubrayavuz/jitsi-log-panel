36fe3cf89eaffce7d71eb991b13810ee02e9d5fc
sdcaglar <seedacaglar@gmail.com>
2021-05-26 09:52:52 +0300

    xmlhttprequests -> axios

--- applications/app/static/js/requests.js
+++ applications/app/static/js/requests.js
@@ -1,26 +1,10 @@
-function request(method, location, headers, data) {
-    r = new XMLHttpRequest();
-    r.open(method, location, false);
-    for (var i = headers.length - 1; i >= 0; i--) {
-        r.setRequestHeader(headers[i][0], headers[i][1]);
-    }
-    r.send(data);
-    response = r.responseText;
-    return response;
-}
-
-
-function async_request(method, location, headers, data, callback) {
-  var r = new XMLHttpRequest();
-  r.open(method, location);
-  header_keys = Object.keys(headers);
-  for (var i = header_keys.length - 1; i >= 0; i--) {
-    r.setRequestHeader(header_keys[i], headers[header_keys[i]]);
-  }
-  r.onreadystatechange = function() {
-    if (r.readyState == 4){
-        callback(r.responseText);
-    }
-  };
-  r.send(data);
+function request(method, url, data) {
+  return axios({
+    method: method,
+    url: url,
+    data: data,
+    timeout: 4000
+  })
+  .then((response) => response.data)
+  .catch(err => console.log(err))
 }
